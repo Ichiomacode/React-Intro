f439c5bb2e23d37b31152c2f5e1b09b0729d33b1
Faustina Ichioma Onoriode <ichiomafaustina@gmail.com>
2023-03-27 08:12:44 -0700

    Introduction to React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Main from './Main';
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+      </>
+  );
+}
+
+export default App;
